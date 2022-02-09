ab5efe71d82f21dfbb73c940cbc3c1d817e22cc8
Kyle Stearns <kjstearns2900@gmail.com>
2022-02-09 14:08:53 -0500

    Attempted to fix flake8 errors

--- main.py
+++ main.py
@@ -43,14 +43,19 @@ def setup_db(cursor:sqlite3.Cursor):
     ratingVotes1 TEXT DEFAULT "none",
     FOREIGN KEY (show_id) REFERENCES table250(show_id)
     );''')
+
+
 def open_db(filename:str)->Tuple[sqlite3.Connection, sqlite3.Cursor]:
-    db_connection = sqlite3.connect(filename)#connect to existing DB or create new one
-    cursor = db_connection.cursor()#get ready to read/write data
+    db_connection = sqlite3.connect(filename)
+    cursor = db_connection.cursor()
     return db_connection, cursor
 
+
 def close_db(connection:sqlite3.Connection):
-    connection.commit()#make sure any changes get saved
+    connection.commit()
     connection.close()
+
+
 def getUserRatings(showId):
     loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/{showId}"
     results = requests.get(loc)
@@ -116,36 +121,41 @@ def writeRatings(dic):
             f.write("percent " + ((dic.get("ratings")[m]).get("percent")) + "\n")
             f.write("votes " + ((dic.get("ratings")[m]).get("votes")) + "\n\n")
 
+
 def add250(cursor:sqlite3.Cursor,tv):
     q1 ="SELECT * FROM table250 WHERE show_id=(?)"
     check = cursor.execute(q1, ("tt5491994",))
     if len(check.fetchall()) == 0:
         q = "INSERT INTO table250(show_id,title,full_title,year,crew,imdb_rating,rating_count) VALUES (?,?,?,?,?,?,?)"
         for i in range(250):
-            cursor.execute(q, (((tv.get("items"))[i]).get("id"),((tv.get("items"))[i]).get("title"),((tv.get("items"))[i]).get("fullTitle"),((tv.get("items"))[i]).get("year"),((tv.get("items"))[i]).get("crew"),((tv.get("items"))[i]).get("imDbRating"),((tv.get("items"))[i]).get("imDbRatingCount")))
+            cursor.execute(q, (((tv.get("items"))[i]).get("id"), ((tv.get("items"))[i]).get("title"), ((tv.get("items"))[i]).get("fullTitle"), ((tv.get("items"))[i]).get("year"), ((tv.get("items"))[i]).get("crew"), ((tv.get("items"))[i]).get("imDbRating"), ((tv.get("items"))[i]).get("imDbRatingCount")))
     else:
         print("already in there")
+
+
 def addRatings(cursor:sqlite3.Cursor,data):
     if len(data.get("ratings")) == 0:
         print("No ratings for " + data.get("imDbId"))
 
     else:
         q = "INSERT INTO ratings(show_id,total_rating,total_rating_votes,rating10percentage,ratingVotes10,rating9percentage,ratingVotes9,rating8percentage,ratingVotes8,rating7percentage,ratingVotes7,rating6percentage,ratingVotes6,rating5percentage,ratingVotes5,rating4percentage,ratingVotes4,rating3percentage,ratingVotes3,rating2percentage,ratingVotes2,rating1percentage,ratingVotes1) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)"
-        cursor.execute(q, (data.get("imDbId"),data.get("totalRating"),data.get("totalRatingVotes"),(data.get("ratings")[0].get("percent")),(data.get("ratings")[0].get("votes")),(data.get("ratings")[1].get("percent")),(data.get("ratings")[1].get("votes")),(data.get("ratings")[2].get("percent")),(data.get("ratings")[2].get("votes")),(data.get("ratings")[3].get("percent")),(data.get("ratings")[3].get("votes")),(data.get("ratings")[4].get("percent")),(data.get("ratings")[4].get("votes")),(data.get("ratings")[5].get("percent")),(data.get("ratings")[5].get("votes")),(data.get("ratings")[6].get("percent")),(data.get("ratings")[6].get("votes")),(data.get("ratings")[7].get("percent")),(data.get("ratings")[7].get("votes")),(data.get("ratings")[8].get("percent")),(data.get("ratings")[8].get("votes")),(data.get("ratings")[9].get("percent")),(data.get("ratings")[9].get("votes"))))
+        cursor.execute(q, (data.get("imDbId"), data.get("totalRating"), data.get("totalRatingVotes"), (data.get("ratings")[0].get("percent")), (data.get("ratings")[0].get("votes")), (data.get("ratings")[1].get("percent")), (data.get("ratings")[1].get("votes")),
+                           (data.get("ratings")[2].get("percent")), (data.get("ratings")[2].get("votes")), (data.get("ratings")[3].get("percent")), (data.get("ratings")[3].get("votes")), (data.get("ratings")[4].get("percent")), (data.get("ratings")[4].get("votes")), (data.get("ratings")[5].get("percent")), (data.get("ratings")[5].get("votes")), (data.get("ratings")[6].get("percent")), (data.get("ratings")[6].get("votes")), (data.get("ratings")[7].get("percent")), (data.get("ratings")[7].get("votes")), (data.get("ratings")[8].get("percent")), (data.get("ratings")[8].get("votes")), (data.get("ratings")[9].get("percent")), (data.get("ratings")[9].get("votes"))))
+
 
 def check250(cursor:sqlite3.Cursor):
     q = "SELECT COUNT(*) FROM table250"
     cursor.execute(q)
     return cursor.fetchall()[0][0]
-def yippie():
-    return
+
+
 def main():
 
     tv = get250Shows()
     data1 = getUserRatings(findIdbyRank("1", tv))
     data50 = getUserRatings(findIdbyRank("50", tv))
     data100 = getUserRatings(findIdbyRank("100", tv))
-    data200 = getUserRatings(findIdbyRank("200",  tv))
+    data200 = getUserRatings(findIdbyRank("200", tv))
     dataWheel = getUserRatings("tt7462410")
 
 #    writeRatings(data1)
@@ -158,17 +168,16 @@ def main():
 
     setup_db(cursor)
     add250(cursor,tv)
-    #addRatings(cursor, data1)
-    #addRatings(cursor, data50)
-    #addRatings(cursor, data100)
-    #addRatings(cursor, data200)
-    #addRatings(cursor, dataWheel)
-# show_simple_select(cursor)
-# show_select_with_join(cursor)
-
-
+    addRatings(cursor, data1)
+    print("done")
+    addRatings(cursor, data50)
+    print("done")
+    addRatings(cursor, data100)
+    print("done")
+    addRatings(cursor, data200)
+    print("done")
+    addRatings(cursor, dataWheel)
     close_db(conn)
-
 
 
 f.close()
--- tests/tests.py
+++ tests/tests.py
@@ -3,39 +3,35 @@ import main
 from typing import Tuple
 
 
-
-
-
-
-
-
 def check250():
+    result = main.check250(cursor) - 1
 
-   result = main.check250(cursor)-1
+    assert result == 250
 
-   assert result == 250
+
+def checkTable():
+    q2 = "SELECT * FROM table250 WHERE show_id=(?)"
+    check = cursor.execute(q2, ("abcdefg",))
+    if len(check.fetchall()) == 0:
+        q = "INSERT INTO table250(show_id,title,full_title,year,crew,imdb_rating,rating_count) VALUES (?,?,?,?,?,?,?)"
+        cursor.execute(q, ("abcdefg", "Sea of these", "Sea of these 2022", "2022",
+                           "Kyle Stearns, Teddy Barnes, Micheal Foley,", "100.4", "12345"))
+
+        q1 = "SELECT COUNT(*) FROM table250"
+        cursor.execute(q1)
+        result = cursor.fetchall()[0][0]
+
+        assert result == 1
+    else:
+        print("already there")
+
 
 conn, cursor = main.open_db("testdata")
-def checkTable():
-   q1 = "SELECT * FROM table250 WHERE show_id=(?)"
-   check = cursor.execute(q1, ("abcdefg",))
-   if len(check.fetchall()) == 0:
-      q = "INSERT INTO table250(show_id,title,full_title,year,crew,imdb_rating,rating_count) VALUES (?,?,?,?,?,?,?)"
-      cursor.execute(q, ("abcdefg","Sea of these","Sea of these 2022","2022","Kyle Stearns, Teddy Barnes, Micheal Foley,","100.4","12345"))
-
-      q1 = "SELECT COUNT(*) FROM table250"
-
-      cursor.execute(q1)
-      result = cursor.fetchall()[0][0]
-
-      assert result == 1
-   else:
-      print("already there")
 main.setup_db(cursor)
 tv = main.get250Shows()
 checkTable()
 
-main.add250(cursor,tv)
+main.add250(cursor, tv)
 
 check250()
 main.close_db(conn)
