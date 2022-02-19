c940cd9d07a89afb2b3f36b4726ee00e552a9f40
Kyle Stearns <kjstearns2900@gmail.com>
2022-02-19 12:59:32 -0500

    Fixed Testing error involving the rank addition to table250

--- tests/tests2.py
+++ tests/tests2.py
@@ -8,14 +8,15 @@ def test_checkTable():
     q2 = "SELECT * FROM table250 WHERE show_id=(?)"
     check = cursor.execute(q2, ("abcdefg",))
     if len(check.fetchall()) == 0:
-        q = "INSERT INTO table250(show_id,rank,title,full_title,year,crew,imdb_rating,rating_count) VALUES (?,?,?,?,?,?,?)"
-        cursor.execute(q, ("abcdefg", "Sea of these", "Sea of these 2022", "2022",
+        q = "INSERT INTO table250(show_id,rank,title,full_title,year,crew,imdb_rating,rating_count) " \
+            "VALUES (?,?,?,?,?,?,?,?)"
+        cursor.execute(q, ("abcdefg", "rank:213", "Sea of these", "Sea of these 2022", "2022",
                            "Kyle Stearns, Teddy Barnes, Micheal Foley,", "100.4", "12345"))
 
         q1 = "SELECT COUNT(*) FROM table250"
         cursor.execute(q1)
         result = cursor.fetchall()[0][0]
 
-        assert result == 251
+        assert result == 1
     else:
         print("already there")
